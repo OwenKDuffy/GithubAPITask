046511f1f1bf39d531f13b50f9c7a93480a71f01
Owen <duffyow@tcd.ie>
2018-12-03 02:31:42 +0000

    Added Virtualenv and bug fixes

--- Interrogation.py
+++ Interrogation.py
@@ -6,10 +6,10 @@ from github import Github
 #g = Github("user", "password")
 
 # or using an access token
-g = Github("159d6c512e9348d8dcb55868790d7f1e613894f3")
+g = Github("a1e025c4f5766c0a2570bf32e513943c465b07f5")
 
 repos = g.get_user().get_repos()
-print("This user has " + repr(len(repos))) + " public Repositories"
+print("This user has " + repr(repos.totalCount) + " public Repositories")
 for r in repos:
     numContents = len(r.get_contents(""))
     print(r.name + " has " + repr(numContents) + " files")
